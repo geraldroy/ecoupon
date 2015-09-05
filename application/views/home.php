ed0a3dd54971fc710d0d7b62b0e1e772a4635ffc
Mireya Andres <mireyagenandres@gmail.com>
2015-09-05 22:35:09 +0800

    Add header.

---
+++ application/views/home.php
@@ -0,0 +1,28 @@
+<?php $this->load->view('layouts/_head'); ?>
+<?php $this->load->view('layouts/_nav'); ?>
+
+<div class="column">
+  <img src="/img/seeds.jpeg" alt="" class="home-img">    
+</div>
+<section id="grid" class="ui segment">
+  <div class="ui stackable grid">
+    <div class="four wide column">
+      <h1>Semantic UI</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, ipsam ad molestias autem iure rem nulla sequi fugiat ea architecto ipsum. Necessitatibus, deleniti. Provident dolor odit iusto, ullam laboriosam. Laudantium!</p>      
+    </div>
+    <div class="four wide column">
+      <h1>Semantic UI</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, ipsam ad molestias autem iure rem nulla sequi fugiat ea architecto ipsum. Necessitatibus, deleniti. Provident dolor odit iusto, ullam laboriosam. Laudantium!</p>      
+    </div>
+    <div class="four wide column">
+      <h1>Semantic UI</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, ipsam ad molestias autem iure rem nulla sequi fugiat ea architecto ipsum. Necessitatibus, deleniti. Provident dolor odit iusto, ullam laboriosam. Laudantium!</p>      
+    </div>
+    <div class="four wide column">
+      <h1>Semantic UI</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, ipsam ad molestias autem iure rem nulla sequi fugiat ea architecto ipsum. Necessitatibus, deleniti. Provident dolor odit iusto, ullam laboriosam. Laudantium!</p>      
+    </div>
+  </div>    
+</section>
+
+<?php $this->load->view('layouts/_footer'); ?>
---
+++ application/views/layouts/_nav.php
@@ -0,0 +1,7 @@
+  <div class="column centered">
+    <div class="ui secondary menu">
+      <a class="item" href="#">How it works?</a>
+      <a class="item" href="#">Products</a>
+      <a class="item" href="#">Advocacy</a>
+    </div>
+  </div>
