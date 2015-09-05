cd73cf25384da2ff0a3e3a3f66dd6c5299284c57
Mireya Andres <mireyagenandres@gmail.com>
2015-09-05 23:04:42 +0800

    Remove active in nav.

--- application/views/layouts/_nav.php
+++ application/views/layouts/_nav.php
@@ -1,6 +1,6 @@
   <div class="column centered">
     <div class="ui secondary menu">
-      <a class="active item" href="how">How it works?</a>
+      <a class="item" href="how">How it works?</a>
       <a class="item" href="products">Products</a>
       <a class="item" href="advocacy">Advocacy</a>
     </div>
