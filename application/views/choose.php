2c0a0bcf3ce3085bd3be9a10cfd4168683b45a8b
Mireya Andres <mireyagenandres@gmail.com>
2015-09-06 03:36:42 +0800

    Added four trees.

--- application/views/choose.php
+++ application/views/choose.php
@@ -15,7 +15,120 @@
       <div class="eight wide column">
         <div class="ui link fluid card">
           <div class="image white-bg">
-            <img src="<?php echo base_url();?>" alt="" class="card-img">
+            <img src="<?php echo base_url();?>/img/banaba.jpg" alt="" class="card-img">
+          </div>
+          <div class="content">
+            <a href="#" class="header">Banaba</a>
+            <div class="meta">
+              <span class="date">Lagerstroemia speciosa</span>
+            </div>
+            <div class="description">
+              Before shedding, the leaves are bright orange or red during which time it is thought to contain higher levels of corosolic acid). Flowers are racemes, pink to lavender; flowering from March to June. After flowering, the tree bears large clumps of oval nutlike fruits.
+              <br><br>
+              Banaba Trees grow in the wild and are widely distributed in the Philippines - in the secondary forests at low and medium altitudes. It is cultivated for its beautiful flowers and propagates by seeds.
+            </div>
+            <div class="extra content">
+              <br>
+              <i class="fa fa-leaf"></i>
+              22 planted
+            </div>
+          </div>
+          <div class="ui bottom attached button plant">
+             Plant a seed!
+          </div>
+        </div>
+      </div>
+
+      <div class="eight wide column">
+        <div class="ui link fluid card">
+          <div class="image white-bg">
+            <img src="<?php echo base_url();?>/img/ilang-ilang.jpg" alt="" class="card-img">
+          </div>
+          <div class="content">
+            <a href="#" class="header">Ylang-Ylang</a>
+            <div class="meta">
+              <span class="date">Cananga odorata,</span>
+            </div>
+            <div class="description">
+                Blooming all year
+                round, the tree yields a large amount of flowers. Ylang ylang trees require extensive pruning, which restricts their growth to two metres high. This
+                process leads to more flowers being produced, which in turn allows more oil to be extracted. A tree that is over five years old will produce 20-25kg of
+                flowers per year.
+                <br><br>
+                The smallest flowers on the plant produce the most subtle perfume, while the wild flowers have little perfume. Starting as green flowers, the ylang
+                ylang plant becomes yellow with a strong perfume 20 days later. Although the plant bears flowers throughout the year, a wet environment encourages further growth, meaning the plant flourishes in the rainy season.            </div>
+            <div class="extra content">
+              <br>
+              <i class="fa fa-leaf"></i>
+              22 planted
+            </div>
+          </div>
+          <div class="ui bottom attached button plant">
+             Plant a seed!
+          </div>
+        </div>
+      </div>
+
+      <div class="eight wide column">
+        <div class="ui link fluid card">
+          <div class="image white-bg">
+            <img src="<?php echo base_url();?>/img/molave.jpeg" alt="" class="card-img">
+          </div>
+          <div class="content">
+            <a href="#" class="header">Molave</a>
+            <div class="meta">
+              <span class="date">Vitex parviflora</span>
+            </div>
+            <div class="description">
+              Molave is a tree that grows irregularly, short, crooked, and has a fluted bole with thick, low, medium, to moderately large buttresses. It is intolerant to shade and a light-loving species with a spreading crown. It partially or entirely sheds its leaves during the latter part of the dry season. The leaves are typically opposite or whorled and mostly simple. The crown usually covers more than half of the total height of the tree with the main branches ascending.
+              The fruit dries, separating at maturity into 24 nutlets. The average number of seeds per fruit ranges from one to three. Its wood is lightly colored, deepening with age; hard to very hard; straight-grained to slightly interlocked or wavy; and fine to moderately fine-textured.
+              <br><br>
+              Molave is common in both secondary and open primary forests at low altitude throughout the Philippines in all or most islands and provinces. Thus, these forests are often called “molave forests”.
+            </div>
+            <div class="extra content">
+              <br>
+              <i class="fa fa-leaf"></i>
+              22 planted
+            </div>
+          </div>
+          <div class="ui bottom attached button plant">
+             Plant a seed!
+          </div>
+        </div>
+      </div>
+
+      <div class="eight wide column">
+        <div class="ui link fluid card">
+          <div class="image white-bg">
+            <img src="<?php echo base_url();?>/img/fire tree.jpg" alt="" class="card-img">
+          </div>
+          <div class="content">
+            <a href="#" class="header">Fire Tree</a>
+            <div class="meta">
+              <span class="date">Delonix regia</span>
+            </div>
+            <div class="description">
+              It is noted for its fern-like leaves and flamboyant display of flowers. In many tropical parts of the world it is grown as an ornamental tree and in English it is given the name Royal Poinciana or Flamboyant. It is also one of several trees known as Flame tree. 
+              The tree's vivid red/vermilion/orange/yellow flowers and bright green foliage make it an exceptionally striking sight.
+              <br><br>
+              In addition to its ornamental value, it is also a useful shade tree in tropical conditions, because it usually grows to a modest height (mostly 5 meters, but it can reach an maximum height of 12 meters) but spreads widely, and its dense foliage provides full shade. In areas with a marked dry season, it sheds its leaves during the drought, but in other areas it is virtually evergreen.
+            </div>
+            <div class="extra content">
+              <br>
+              <i class="fa fa-leaf"></i>
+              22 planted
+            </div>
+          </div>
+          <div class="ui bottom attached button plant">
+             Plant a seed!
+          </div>
+        </div>
+      </div>
+
+      <div class="eight wide column">
+        <div class="ui link fluid card">
+          <div class="image white-bg">
+            <img src="<?php echo base_url();?>/img/." alt="" class="card-img">
           </div>
           <div class="content">
             <a href="#" class="header">Tree</a>
@@ -40,7 +153,7 @@
       <div class="eight wide column">
         <div class="ui link fluid card">
           <div class="image white-bg">
-            <img src="<?php echo base_url();?>" alt="" class="card-img">
+            <img src="<?php echo base_url();?>/img/." alt="" class="card-img">
           </div>
           <div class="content">
             <a href="#" class="header">Tree</a>
@@ -65,7 +178,7 @@
       <div class="eight wide column">
         <div class="ui link fluid card">
           <div class="image white-bg">
-            <img src="<?php echo base_url();?>" alt="" class="card-img">
+            <img src="<?php echo base_url();?>/img/." alt="" class="card-img">
           </div>
           <div class="content">
             <a href="#" class="header">Tree</a>
@@ -90,107 +203,7 @@
       <div class="eight wide column">
         <div class="ui link fluid card">
           <div class="image white-bg">
-            <img src="<?php echo base_url();?>" alt="" class="card-img">
-          </div>
-          <div class="content">
-            <a href="#" class="header">Tree</a>
-            <div class="meta">
-              <span class="date">Scientific Name</span>
-            </div>
-            <div class="description">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero itaque beatae aliquid aperiam ea sapiente repudiandae, repellat consequuntur, quos fugiat in, nesciunt sed. Iure laboriosam soluta inventore porro, sint cumque.
-            </div>
-            <div class="extra content">
-              <br>
-              <i class="fa fa-leaf"></i>
-              22 planted
-            </div>
-          </div>
-          <div class="ui bottom attached button plant">
-             Plant a seed!
-          </div>
-        </div>
-      </div>
-
-      <div class="eight wide column">
-        <div class="ui link fluid card">
-          <div class="image white-bg">
-            <img src="<?php echo base_url();?>" alt="" class="card-img">
-          </div>
-          <div class="content">
-            <a href="#" class="header">Tree</a>
-            <div class="meta">
-              <span class="date">Scientific Name</span>
-            </div>
-            <div class="description">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero itaque beatae aliquid aperiam ea sapiente repudiandae, repellat consequuntur, quos fugiat in, nesciunt sed. Iure laboriosam soluta inventore porro, sint cumque.
-            </div>
-            <div class="extra content">
-              <br>
-              <i class="fa fa-leaf"></i>
-              22 planted
-            </div>
-          </div>
-          <div class="ui bottom attached button plant">
-             Plant a seed!
-          </div>
-        </div>
-      </div>
-
-      <div class="eight wide column">
-        <div class="ui link fluid card">
-          <div class="image white-bg">
-            <img src="<?php echo base_url();?>" alt="" class="card-img">
-          </div>
-          <div class="content">
-            <a href="#" class="header">Tree</a>
-            <div class="meta">
-              <span class="date">Scientific Name</span>
-            </div>
-            <div class="description">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero itaque beatae aliquid aperiam ea sapiente repudiandae, repellat consequuntur, quos fugiat in, nesciunt sed. Iure laboriosam soluta inventore porro, sint cumque.
-            </div>
-            <div class="extra content">
-              <br>
-              <i class="fa fa-leaf"></i>
-              22 planted
-            </div>
-          </div>
-          <div class="ui bottom attached button plant">
-             Plant a seed!
-          </div>
-        </div>
-      </div>
-
-      <div class="eight wide column">
-        <div class="ui link fluid card">
-          <div class="image white-bg">
-            <img src="<?php echo base_url();?>" alt="" class="card-img">
-          </div>
-          <div class="content">
-            <a href="#" class="header">Tree</a>
-            <div class="meta">
-              <span class="date">Scientific Name</span>
-            </div>
-            <div class="description">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero itaque beatae aliquid aperiam ea sapiente repudiandae, repellat consequuntur, quos fugiat in, nesciunt sed. Iure laboriosam soluta inventore porro, sint cumque.
-            </div>
-            <div class="extra content">
-              <br>
-              <i class="fa fa-leaf"></i>
-              22 planted
-            </div>
-          </div>
-          <div class="ui bottom attached button plant">
-             Plant a seed!
-          </div>
-        </div>
-      </div>
-
-      <div class="eight wide column">
-        <div class="ui link fluid card">
-          <div class="image white-bg">
-            <img src="<?php echo base_url();?>" alt="" class="card-img">
+            <img src="<?php echo base_url();?>/img/." alt="" class="card-img">
           </div>
           <div class="content">
             <a href="#" class="header">Tree</a>
