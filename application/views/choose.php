a33eaa9882b68e82b23ee29bfcc1510ebfc981a6
Mireya Andres <mireyagenandres@gmail.com>
2015-09-06 00:43:47 +0800

    Add template for trees.

--- application/views/choose.php
+++ application/views/choose.php
@@ -1,1 +1,152 @@
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa minima deleniti similique, nobis qui doloribus sint rem aut iure libero mollitia temporibus vel totam possimus nesciunt, recusandae amet a velit.
+<?php $this->load->view('layouts/_head'); ?>
+<?php $this->load->view('layouts/_nav'); ?>
+  <script src="<?php echo base_url(); ?>/js/jquery.js"></script>
+  <script src="<?php echo base_url(); ?>/js/semantic.min.js"></script>
+  <script src="<?php echo base_url(); ?>/js/app.js"></script>
+
+<div class="desc column">
+  <h1>Choose a Tree!</h1>
+  <p>And our partner NGOs will plan it!</p>
+</div>
+
+<div class="">
+  <div class="cont">
+    <div class="ui cards stackable grid">
+      <div class="eight wide column">
+        <div class="ui link fluid card">
+          <div class="image">
+            <img src="<?php echo base_url();?>" alt="">
+          </div>
+          <div class="content">
+            <a href="#" class="header">Tree</a>
+            <div class="meta">
+              <span class="date">Scientific Name</span>
+            </div>
+            <div class="description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero itaque beatae aliquid aperiam ea sapiente repudiandae, repellat consequuntur, quos fugiat in, nesciunt sed. Iure laboriosam soluta inventore porro, sint cumque.
+            </div>
+          <div class="extra content">
+            <br>
+            <i class="fa fa-leaf"></i>
+            22 planted
+          </div>        
+          </div>
+        </div>        
+      </div>
+
+      <div class="eight wide column">
+        <div class="ui link fluid card">
+          <div class="image">
+            <img src="<?php echo base_url();?>" alt="">
+          </div>
+          <div class="content">
+            <a href="#" class="header">Tree</a>
+            <div class="meta">
+              <span class="date">Scientific Name</span>
+            </div>
+            <div class="description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero itaque beatae aliquid aperiam ea sapiente repudiandae, repellat consequuntur, quos fugiat in, nesciunt sed. Iure laboriosam soluta inventore porro, sint cumque.
+            </div>
+          <div class="extra content">
+            <br>
+            <i class="fa fa-leaf"></i>
+            22 planted
+          </div>        
+          </div>
+        </div>        
+      </div>
+
+      <div class="eight wide column">
+        <div class="ui link fluid card">
+          <div class="image">
+            <img src="<?php echo base_url();?>" alt="">
+          </div>
+          <div class="content">
+            <a href="#" class="header">Tree</a>
+            <div class="meta">
+              <span class="date">Scientific Name</span>
+            </div>
+            <div class="description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero itaque beatae aliquid aperiam ea sapiente repudiandae, repellat consequuntur, quos fugiat in, nesciunt sed. Iure laboriosam soluta inventore porro, sint cumque.
+            </div>
+          <div class="extra content">
+            <br>
+            <i class="fa fa-leaf"></i>
+            22 planted
+          </div>        
+          </div>
+        </div>        
+      </div>
+
+      <div class="eight wide column">
+        <div class="ui link fluid card">
+          <div class="image">
+            <img src="<?php echo base_url();?>" alt="">
+          </div>
+          <div class="content">
+            <a href="#" class="header">Tree</a>
+            <div class="meta">
+              <span class="date">Scientific Name</span>
+            </div>
+            <div class="description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero itaque beatae aliquid aperiam ea sapiente repudiandae, repellat consequuntur, quos fugiat in, nesciunt sed. Iure laboriosam soluta inventore porro, sint cumque.
+            </div>
+          <div class="extra content">
+            <br>
+            <i class="fa fa-leaf"></i>
+            22 planted
+          </div>        
+          </div>
+        </div>        
+      </div>
+
+      <div class="eight wide column">
+        <div class="ui link fluid card">
+          <div class="image">
+            <img src="<?php echo base_url();?>" alt="">
+          </div>
+          <div class="content">
+            <a href="#" class="header">Tree</a>
+            <div class="meta">
+              <span class="date">Scientific Name</span>
+            </div>
+            <div class="description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero itaque beatae aliquid aperiam ea sapiente repudiandae, repellat consequuntur, quos fugiat in, nesciunt sed. Iure laboriosam soluta inventore porro, sint cumque.
+            </div>
+          <div class="extra content">
+            <br>
+            <i class="fa fa-leaf"></i>
+            22 planted
+          </div>        
+          </div>
+        </div>        
+      </div>
+
+      <div class="eight wide column">
+        <div class="ui link fluid card">
+          <div class="image">
+            <img src="<?php echo base_url();?>" alt="">
+          </div>
+          <div class="content">
+            <a href="#" class="header">Tree</a>
+            <div class="meta">
+              <span class="date">Scientific Name</span>
+            </div>
+            <div class="description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero itaque beatae aliquid aperiam ea sapiente repudiandae, repellat consequuntur, quos fugiat in, nesciunt sed. Iure laboriosam soluta inventore porro, sint cumque.
+            </div>
+          <div class="extra content">
+            <br>
+            <i class="fa fa-leaf"></i>
+            22 planted
+          </div>        
+          </div>
+        </div>        
+      </div>
+
+    </div>    
+  </div>
+</div>
+
+<?php $this->load->view('layouts/_footer'); ?>
+
--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   $("#submit-code").click(function(){
     var base_url = window.location.origin;
-    alert(base_url);
     window.location.replace(base_url + "/choose");
   });
 });
