8ff755be24b7fecc077a739485ab1d2768bfabb5
Mireya Andres <mireyagenandres@gmail.com>
2015-09-06 11:29:29 +0800

    Checkpoint.

--- application/views/how.php
+++ application/views/how.php
@@ -9,14 +9,42 @@
   <h3>
     Look for our stickers on eco-friendly and local products.
   </h3>
-  <img src="" alt="">
+  <img src="<?php echo base_url(); ?>/img/step1.jpg" alt="">
 </div>
 
 <div class="cont">
   <h3>
-    Great! Now you planted a tree.
+    Great! With your help, NGOs will get funded for their cause.
   </h3>
-  <img src="" alt="">
+</div>
+
+
+<div class="cont">
+  <h3>
+    See that code on the sticker?
+  </h3>
+  <img src="<?php echo base_url(); ?>/img/logocode.png" alt="" class="resize">
+</div>
+
+<div class="cont">
+  <h3>
+    Enter the code on our site.
+  </h3>
+  <img src="<?php echo base_url(); ?>/img/logocode.png" alt="" class="resize">
+</div>
+
+<div class="cont">
+  <h3>
+    Pick a tree you want to sponsor.
+  </h3>
+  <img src="<?php echo base_url(); ?>/img/logocode.png" alt="" class="resize">
+</div>
+
+
+<div class="cont">
+  <h3>
+    Congrats! An NGO will plant your tree on your behalf! :)
+  </h3>
 </div>
 
 
