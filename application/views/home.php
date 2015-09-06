9009a64b853552c0547c6ffe36140147f5887136
Mireya Andres <mireyagenandres@gmail.com>
2015-09-06 08:15:00 +0800

    Updated version.

--- application/views/home.php
+++ application/views/home.php
@@ -3,6 +3,7 @@
   <script src="<?php echo base_url(); ?>/js/jquery.js"></script>
   <script src="<?php echo base_url(); ?>/js/semantic.min.js"></script>
   <script src="<?php echo base_url(); ?>/js/app.js"></script>
+  <script src="<?php echo base_url(); ?>/js/fullpage.js"></script>
 
 <div class="sixteen wide column">
   <div class="absolute">
@@ -12,30 +13,27 @@
       <input type="text" placeholder="Enter code..." id="code">
       <button class="ui primary button" id="submit-code">Go</div>
     </div>
-    
   </div>
 </div>
-
 
 <div class="column">
   <img src="<?php echo base_url(); ?>/img/seeds.jpeg" alt="" class="home-img">
 </div>
-
 </div>
 
 <div class="cont">
   <div class="ui stackable grid">
     <div class="three column row">
       <div class="column">
-        <h1>Semantic UI</h1>
+        <h1>200+ Trees</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, ipsam ad molestias autem iure rem nulla sequi fugiat ea architecto ipsum. Necessitatibus, deleniti. Provident dolor odit iusto, ullam laboriosam. Laudantium!</p>
       </div>
       <div class="column">
-        <h1>Semantic UI</h1>
+        <h1>20+ NGOs</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, ipsam ad molestias autem iure rem nulla sequi fugiat ea architecto ipsum. Necessitatibus, deleniti. Provident dolor odit iusto, ullam laboriosam. Laudantium!</p>
       </div>
       <div class="column">
-        <h1>Semantic UI</h1>
+        <h1>... more causes!</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, ipsam ad molestias autem iure rem nulla sequi fugiat ea architecto ipsum. Necessitatibus, deleniti. Provident dolor odit iusto, ullam laboriosam. Laudantium!</p>
       </div>
     </div>
